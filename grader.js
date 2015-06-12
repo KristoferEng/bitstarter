54901864e6eef080ff3f23c606563887fab9f46d
Kristofer Eng <slikqaz@gmail.com>
2015-06-12 17:55:01 +0000

    Change per lecture

--- grader.js
+++ grader.js
@@ -3,18 +3,14 @@
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
-
 References:
-
  + cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
    - http://maxogden.com/scraping-with-node.html
-
  + commander.js
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
@@ -24,16 +20,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest=require('restler');
-var sys=require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT="oops"
 
 var assertFileExists = function(infile) {
-    
     var instr = infile.toString();
-    
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
@@ -41,10 +33,8 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
- 
 };
 
 var loadChecks = function(checksfile) {
@@ -68,29 +58,43 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkRemoteHtml = function(url, checksfile, callback) {
+    rest.get(url).on('complete', function(result, response) {
+	if(result instanceof Error) {
+	        console.error('Error: ' + util.format(response.message));
+	        process.exit(1);
+	    } else {
+		    $ = cheerio.load(result);
+		    var checks = loadChecks(checksfile).sort();
+		    var out = {};
+		    for(var ii in checks) {
+			var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+			    }
+		    callback(out);
+		}
+    });
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_address>', 'url value',URL_DEFAULT)
-        .parse(process.argv);     
-    if(program.url!='oops'){
-	rest.get(program.url).on('complete', function(data,response) {         
-            fs.writeFileSync('newindex.html',data);
-            program.file='newindex.html';
-            
-            var checkJson = checkHtmlFile(program.file, program.checks);
-            var outJson = JSON.stringify(checkJson, null, 4);
-            console.log(outJson);
-            
-          
-        });
+        .option('-u, --url <url>', 'URL to index.html')
+    .parse(process.argv);
+
+    var checkJson, outJson;
+    if(program.url) {
+	checkRemoteHtml(program.url, program.checks, function(checkJson){
+	        outJson = JSON.stringify(checkJson, null, 4);
+	        console.log(outJson);
+	    });
+    } else {
+	checkJson = checkHtmlFile(program.file, program.checks);
+	outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
     }
-    else{    
-       var checkJson = checkHtmlFile(program.file, program.checks);
-       var outJson = JSON.stringify(checkJson, null, 4);
-       console.log(outJson);
-    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
